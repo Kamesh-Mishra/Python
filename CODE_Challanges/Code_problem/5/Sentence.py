82c6fac2a9dde9aa5e0eff4ea351091ae96dd77a
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-08-22 15:35:10 +0530

    Added new file

---
+++ CODE_Challanges/Code_problem/5/Sentence.py
@@ -0,0 +1,46 @@
+
+"""
+Name: 
+    Sentence       
+Filename:
+    Sentence.py
+Problem Statement:
+    You are given a sentence, and want to shift each letter by 2 in alphabet to create a secret code. 
+    The sentence you want to encode is the lazy dog jumped over the quick brown 
+    fox and the output should be ’vjg ncba fqi lworgf qxgt vjg swkem dtqyp hqz’ 
+Data:
+    Not required
+Extension:
+    Not Available   
+Hint: 
+    Not Available 
+Algorithm:
+    Create a dictionary mapping each letter to its number in the alphabet
+    Create a dictionary mapping each number to its letter in the alphabet
+    Go through each letter in the sentence and find the corresponding number, add 2 and then find the new corresponding letter
+    Make sure to take care of the edge cases so that if you get the letter z, it maps to b… ect
+    Print the encoded string  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    Not Available
+Sample Output:
+    Not Available
+""" 
+
+ 
+str1 = "abcdefghijklmnopqrstuvwxyz"
+str2 = "cdefghijklmnopqrstuvwxyzab"
+str3 = ""
+dict1 = {}
+for i in range(len(str1)):
+    dict1[str1[i]] = str2[i]
+print(dict1)
+
+inp = input("enter sentence: ")
+for j in inp:
+    if j in dict1.keys():
+        str3 = str3+dict1[j]
+    else:
+        str3 = str3+j
+print(str3)
