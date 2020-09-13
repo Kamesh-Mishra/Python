d4ad4ec0a4e25684de78ef1a5f4438ae42c5f456
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-09-13 17:20:41 +0530

    Added new file

---
+++ CODE_Challanges/Code_problem/5/reverse.py
@@ -0,0 +1,32 @@
+
+"""
+Name: 
+    Reverse Function         
+Filename:
+    reverse.py
+Problem Statement:
+    Define a function reverse() that computes the reversal of a string.
+    Without using Python's inbuilt function
+    Take input from User
+Data:
+    Not required
+Extension:
+    Not Available  
+Hint: 
+    Not Available 
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    I am testing
+Sample Output:
+    gnitset ma I
+"""
+
+def reverse():
+    inp = input("Enter input string: ")
+
+    inp = inp[::-1]
+    return(inp)
+reverse()
